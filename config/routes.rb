d15dc31df5727df07019104c080651bad69a68c6
& <pair+admiralrico.haileyr@devbootcamp.com>
2015-01-14 15:00:52 -0500

    Set root and register routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get '/register' => 'pages#register_form', as: :register
+  resources :users
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
